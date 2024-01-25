715e0b979f2d7acbb885b5531c70ed599632af97
Kaivan Kamali <kxk302@psu.edu>
2024-01-24 20:39:38 -0500

    Moved Python script to script folder. Added CLI to shell script

--- scripts/run_g4hunter_primates.sh
+++ scripts/run_g4hunter_primates.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#species_list="Gorilla_gorilla Pan_troglodytes Symphalangus_syndactylus Homo_sapiens Pongo_abelii Pan_paniscus Pongo_pygmaeus"
-species_list="Gorilla_gorilla Pan_troglodytes Symphalangus_syndactylus Pongo_abelii Pan_paniscus Pongo_pygmaeus"
-chromosome_list="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 X Y"
-window_size=25
-score=1.5
-input_folder="./input"
-output_folder="./output"
+#species_list=${1:-"Gorilla_gorilla Pan_troglodytes Symphalangus_syndactylus Homo_sapiens Pongo_abelii Pan_paniscus Pongo_pygmaeus"}
+species_list=${1:-"Homo_sapiens"}
+chromosome_list=${2:-"1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 X Y"}
+window_size=${3:-"25"}
+score=${4:-"1.5"}
+input_folder=${5:-"./input"}
+output_folder=${6:-"./output"}
 
 for species in ${species_list}
 do
@@ -14,7 +14,7 @@ do
   do 
     if [ -f ${input_folder}/${species}/chr${chr}.fasta ]; then
       echo "Running G4Hunter for ${species}, chr${chr}"
-      python2 G4Hunter.py -i ${input_folder}/${species}/chr${chr}.fasta -o ${output_folder}/${species}/ -w ${window_size} -s ${score} 
+      python2 ./scripts/G4Hunter.py -i ${input_folder}/${species}/chr${chr}.fasta -o ${output_folder}/${species}/ -w ${window_size} -s ${score}
     fi
   done
 done
